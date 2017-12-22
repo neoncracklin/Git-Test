69fa7c65eb5766501771f8d2ba57357b72e9c36f
Scott Veronie <scottveronie@2600-6c63-6b7f-f68a-1089-c231-2509-eee5.dhcp6.chtrptr.net>
2017-12-22 15:50:59 -0600

    Added Map Class

---
+++ Git Test/MapClass.swift
@@ -0,0 +1,13 @@
+//
+//  MapClass.swift
+//  Git Test
+//
+//  Created by Scott Veronie on 12/22/17.
+//  Copyright © 2017 Alien Bacon. All rights reserved.
+//
+
+import UIKit
+
+class MapClass: NSObject {
+
+}
