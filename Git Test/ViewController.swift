0cd803766db220326db6da5ec8f13950cd0b8c4a
Scott Veronie <scottveronie@2600-6c63-6b7f-f68a-1089-c231-2509-eee5.dhcp6.chtrptr.net>
2017-12-22 15:27:21 -0600

    Initial Commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Scott Veronie on 12/22/17.
+//  Copyright © 2017 Alien Bacon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
